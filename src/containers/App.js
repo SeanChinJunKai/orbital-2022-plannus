10a735d9aff0464e6e31d1d223a77b37194bdcd2
Orqoi <lewklconrad@gmail.com>
2022-05-20 04:19:37 +0800

    Removed unnecessary skeleton files, updated file structure and added custom font

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import SideBar from './components/SideBar.js';
-import NavBar from './components/NavBar.js';
-import ForumApp from './components/ForumApp.js';
+import '../assets/App.css';
+import SideBar from '../components/SideBar.js';
+import NavBar from '../components/NavBar.js';
+import ForumApp from '../components/ForumApp.js';
 
 function App() {
   return (
