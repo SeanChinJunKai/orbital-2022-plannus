04e257089dd599dac200de0669678a7b5e7cb551
Orqoi <lewklconrad@gmail.com>
2022-05-25 03:48:48 +0800

    Updated requirements page

--- src/containers/PlannerPage/RequirementsApp.js
+++ src/containers/PlannerPage/RequirementsApp.js
@@ -8,23 +8,23 @@ function RequirementsApp(props) {
             subHeadingTitle: 'University Level Requirements',
             subHeadingTotalCredits: 24,
             modules: [{
-                name: 'Programming Methodology (Digital Literacy)',
+                name: 'Programming Methodology',
                 code: 'CS1101S',
                 credits: 4,
                 color: 'darkblue',
             }, {
-                name: 'Cultures and Connections',
-                code: 'GEC%',
+                name: 'Film Art and Human Concerns',
+                code: 'GEC1017',
                 credits: 4,
                 color: 'darkblue',
             }, {
-                name: 'Data Literacy',
-                code: 'Either GEA1000, BT1101, ST1131 or DSE1101',
+                name: 'Quantitative Reasoning with Data',
+                code: 'GEA1000',
                 credits: 4,
                 color: 'darkblue',
             }, {
-                name: 'Singapore Studies',
-                code: 'GES%',
+                name: 'Public Health in Singapore: Challenges and Changes',
+                code: 'GES1042',
                 credits: 4,
                 color: 'darkblue',
             }, {
@@ -33,8 +33,8 @@ function RequirementsApp(props) {
                 credits: 4,
                 color: 'darkblue',
             }, {
-                name: 'Communities and Engagement (Critique and Expression)',
-                code: 'GEN%',
+                name: 'Communities and Engagement',
+                code: 'GEN1512',
                 credits: 4,
                 color: 'darkblue',
             }]
@@ -51,18 +51,18 @@ function RequirementsApp(props) {
             subHeadingTitle: 'Interdisciplinary & Cross-Disciplinary Education',
             subHeadingTotalCredits: 12,
             modules: [{
-                name: 'Digital Ethics and Data Privacy',
-                code: 'IS1108',
+                name: 'IT, Management and Organisation',
+                code: 'IS1128',
                 credits: 4,
                 color: 'darkblue',
             }, {
-                name: 'Digital Ethics and Data Privacy',
-                code: 'IS1108',
+                name: 'Asian Interconnections',
+                code: 'HSA1000',
                 credits: 4,
                 color: 'darkblue',
             }, {
-                name: 'Digital Ethics and Data Privacy',
-                code: 'IS1108',
+                name: 'From DNA to Gene Therapy',
+                code: 'HSI2003',
                 credits: 4,
                 color: 'darkblue',
             }]
@@ -73,15 +73,105 @@ function RequirementsApp(props) {
         subHeadings: [{
             subHeadingTitle: 'Computer Science Foundation',
             subHeadingTotalCredits: 36,
-            modules: []
+            modules: [{
+                name: 'Discrete Structures',
+                code: 'CS1231S',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Programming Methodology II',
+                code: 'CS2030S',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Data Structures and Algorithms',
+                code: 'CS2040S',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Computer Organisation',
+                code: 'CS2100',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Effective Communication for Computing Professionals',
+                code: 'CS2101',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Software Engineering',
+                code: 'CS2103T',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Introduction to Operating Systems',
+                code: 'CS2106',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Introduction to AI and Machine Learning',
+                code: 'CS2109S',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Design and Analysis of Algorithms',
+                code: 'CS3230',
+                credits: 4,
+                color: 'darkblue',
+            },]
         }, {
             subHeadingTitle: 'Computer Science Breadth and Depth',
             subHeadingTotalCredits: 32,
-            modules: []
+            modules: [{
+                name: 'Internship',
+                code: 'CP3200',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Internship II',
+                code: 'CP3202',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Foundations of Software Engineering',
+                code: 'CS3213',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Software Engineering Principles and Patterns',
+                code: 'CS3219',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Formal Methods for Software Engineering',
+                code: 'CS4211',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'B.Comp. Dissertation',
+                code: 'CP4101',
+                credits: 12,
+                color: 'darkblue',
+            }, ]
         }, {
             subHeadingTitle: 'Mathematics and Sciences',
             subHeadingTotalCredits: 12,
-            modules: []
+            modules: [{
+                name: 'Calculus for Computing',
+                code: 'MA1521',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Linear Algebra I',
+                code: 'MA2001',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Probability and Statistics',
+                code: 'ST2334',
+                credits: 4,
+                color: 'darkblue',
+            },]
         }]
     }, {
         heading: 'Unrestricted Electives',
@@ -89,7 +179,57 @@ function RequirementsApp(props) {
         subHeadings: [{
             subHeadingTitle: 'Unrestricted Electives',
             subHeadingTotalCredits: 40,
-            modules: []
+            modules: [{
+                name: 'Fundamental Biochemistry',
+                code: 'LSM2106',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Human Anatomy',
+                code: 'LSM2212',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Cell Biology',
+                code: 'LSM2233',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Introductory Bioinformatics',
+                code: 'LSM2241',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Ecology and Environment',
+                code: 'LSM2251',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Biodiversity',
+                code: 'LSM2252',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Project Finance',
+                code: 'PF2205',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Event Management',
+                code: 'PF2305',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'Principles and Approaches to Design',
+                code: 'ID1223',
+                credits: 4,
+                color: 'darkblue',
+            }, {
+                name: 'The Art of Imaginative Sketching',
+                code: 'ID2117',
+                credits: 4,
+                color: 'darkblue',
+            }, ]
         }]
     }]
     
@@ -97,15 +237,32 @@ function RequirementsApp(props) {
     <div className='RequirementsApp'>
         <div className='PlannerHeader RequirementsHeader'>
             <h1>Course: CS</h1>
-            <h1>Total MC Count: 160</h1>
-            <h1>Eligible for Graduation: Yes</h1>
         </div>
         <div className='RequirementsBody'>
-            {requirements.map(requirement => 
+            {requirements.map(requirement =>
             <div className='requirement-container'>
                 <h1>{requirement.heading}</h1>
-                {requirement.subHeadings.map(subheading => <h3>{subheading.subHeadingTitle}</h3>)}
+                {requirement.subHeadings.map(subheading => 
+                <div className='subrequirement-container'>
+                    <h3>{subheading.subHeadingTitle}</h3>
+                    <div className='subrequirement-modules-container'>
+                        {subheading.modules.map(module =>
+                            <h3>{module.code} {module.name} {module.credits} MC</h3>
+                        )}
+                    </div>
+                    <div className='subrequirement-met-container'>
+                        {subheading.modules.map(module =>
+                            <div className='subrequirement-met-buttons'>
+                                <h3>No</h3>
+                                <button>Add to: </button>
+                            </div>
+                            
+                        )}
+                    </div>
+                </div>
+                )}
             </div>)}
+            
         </div>
         <div className='RequirementsFooter'>
             <h3><a href='default.com' onClick={(e) => {
