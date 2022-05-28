f4e6200ca8e9d46d22b6c7723b5dab47120e2a1f
Orqoi <lewklconrad@gmail.com>
2022-05-28 22:52:28 +0800

    Updated login page styles

--- frontend/src/containers/LoginPage.js
+++ frontend/src/containers/LoginPage.js
@@ -1,13 +1,11 @@
-import {useState, useEffect} from 'react' 
-import {useSelector, useDispatch} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
-import {toast} from 'react-toastify'
-import {login, reset} from "../features/auth/authSlice"
-import Spinner from '../components/Spinner'
+import {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {Link, useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {login, reset} from "../features/auth/authSlice";
+import Spinner from '../components/Spinner';
 import '../assets/Login.css';
-import { Button, TextField } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-import teamLogo from '../assets/PlanNUS.png'
+import teamLogo from '../assets/PlanNUS.png';
 
 
 
@@ -53,47 +51,54 @@ export default function LoginPage() {
       
   }
 
+  const [showPassword, setShowPassword] = useState(false);
+
   if(isLoading) {
       return <Spinner />
   }
 
 
   return (
-  <div className='LoginPage'>
-    <div className='container'>
-        <section className='heading'>
-            <img src={teamLogo} alt='PlanNUS Logo'/>
-        </section>
-        <section className = "form">
-            <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                <input 
-                        type = 'email' 
-                        className ='form-control' 
-                        id='email' 
-                        name = 'email' 
-                        value = {email} 
-                        placeholder='Enter your email' 
-                        onChange = {onChange}/>
-                </div>
-                <div className='form-group'>
-                <input 
-                        type = 'password' 
-                        className ='form-control' 
-                        id='password' 
-                        name = 'password' 
-                        value = {password} 
-                        placeholder='Enter your password' 
-                        onChange = {onChange}/>
-                </div>
-                <div className='button'>
-                    <button type="submit" className='btn btn-block'>Submit</button>
-                </div>
-            </form>
-        </section>
+    <div className='LoginPage'>
+        <div className='login-container'>
+            <section className='login-heading'>
+                <img src={teamLogo} alt='PlanNUS Logo'/>
+            </section>
+            <section className="login-form-container">
+                <form onSubmit={onSubmit} className='login-form'>
+                    <div className='form-group'>
+                        <input 
+                            type = 'email' 
+                            className ='form-control' 
+                            id='email' 
+                            name = 'email' 
+                            value = {email} 
+                            placeholder='Email' 
+                            onChange = {onChange}/>
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                            type = {showPassword ? 'text' : 'password'} 
+                            className ='form-control' 
+                            id='password' 
+                            name = 'password' 
+                            value = {password} 
+                            placeholder='Password' 
+                            onChange = {onChange}/>
+                    </div>
+                    <div className='show-password'>
+                        <input type="checkbox" id="show" name="show" value="show" onClick={() => setShowPassword(!showPassword)}/> Show Password
+                    </div>
+                    <div className='login-buttons'>
+                        <Link to='/Register'>Register</Link>
+                        {/*<Link to='/Forgot'>Forgot Password?</Link>
+                            To be updated*/}
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </section>
+        </div>
     </div>
-</div>)
+    )
   
 }
-
-
--- frontend/src/containers/RegisterPage.js
+++ frontend/src/containers/RegisterPage.js
@@ -1,11 +1,10 @@
 import {useState, useEffect} from 'react' 
-import {FaUser} from 'react-icons/fa'
 import {useSelector, useDispatch} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
 import {register, reset} from "../features/auth/authSlice"
 import Spinner from '../components/Spinner'
-import '../assets/Register.css';
+import '../assets/Login.css';
 
 function RegisterPage() {
   const[formData, setFormData] = useState({
@@ -55,63 +54,62 @@ function RegisterPage() {
       return <Spinner />
   }
 
-  return (<div className='RegisterPage'>
-  <div className='container'>
-    <section className='heading'>
-        <h1>
-            <FaUser /> Register
-        </h1>
-    </section>
-    <section className = "form">
-        <form onSubmit={onSubmit}>
-            <div className='form-group'>
-                <input 
-                    type = 'text' 
-                    className ='form-control' 
-                    id='name' 
-                    name = 'name' 
-                    value = {name} 
-                    placeholder='Enter your name' 
-                    onChange = {onChange}/>
-            </div>
-            <div className='form-group'>
-                <input 
-                    type = 'email' 
-                    className ='form-control' 
-                    id='email' 
-                    name = 'email' 
-                    value = {email} 
-                    placeholder='Enter your email' 
-                    onChange = {onChange}/>
-            </div>
-            <div className='form-group'>
-                <input 
-                    type = 'password' 
-                    className ='form-control' 
-                    id='password' 
-                    name = 'password' 
-                    value = {password} 
-                    placeholder='Enter your password' 
-                    onChange = {onChange}/>
-            </div>
-            <div className='form-group'>
-                <input 
-                    type = 'password' 
-                    className ='form-control' 
-                    id='password2' 
-                    name = 'password2' 
-                    value = {password2} 
-                    placeholder='Confirm password' 
-                    onChange = {onChange}/>
-            </div>
-            <div className='form-group'>
-                <button type="submit" className='btn btn-block'>Submit</button>
-            </div>
-
-        </form>
-    </section>
+  return (
+    <div className='RegisterPage'>
+        <div className='register-container'>
+            <section className='register-heading'>
+                <h1>Register</h1>
+            </section>
+            <section className="register-form-container">
+                <form className='register-form' onSubmit={onSubmit}>
+                    <div className='form-group'>
+                        <input 
+                            type = 'text' 
+                            className ='form-control' 
+                            id='name' 
+                            name = 'name' 
+                            value = {name} 
+                            placeholder='Enter your name' 
+                            onChange = {onChange}/>
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                            type = 'email' 
+                            className ='form-control' 
+                            id='email' 
+                            name = 'email' 
+                            value = {email} 
+                            placeholder='Enter your email' 
+                            onChange = {onChange}/>
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                            type = 'password' 
+                            className ='form-control' 
+                            id='password' 
+                            name = 'password' 
+                            value = {password} 
+                            placeholder='Enter your password' 
+                            onChange = {onChange}/>
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                            type = 'password' 
+                            className ='form-control' 
+                            id='password2' 
+                            name = 'password2' 
+                            value = {password2} 
+                            placeholder='Confirm password' 
+                            onChange = {onChange}/>
+                    </div>
+                    <div className='register-buttons'>
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+            </section>
+        </div>
     </div>
-  </div>)
+    )
 }
 
 export default RegisterPage
