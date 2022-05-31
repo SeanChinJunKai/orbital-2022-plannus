309649d802244609faad679787f6cadb312eb72d
SeanChinJunKai <seanchinjk1@gmail.com>
2022-05-31 13:27:17 +0800

    Added some code comments

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
+// importing mongoose which is a MongoDB object modelling tool designed to work in asynchronous environment
 const mongoose = require('mongoose')
 
+
+// function which connects to the MongoDB database via the uri given in .env file
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
@@ -11,3 +14,5 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB
+
+// More info on async and await : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
