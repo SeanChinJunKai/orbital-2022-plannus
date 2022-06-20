73327ee7d24d6949f9a6a6382e4d0c8e478ec83b
Orqoi <lewklconrad@gmail.com>
2022-06-20 18:24:25 +0800

    Created report page with functionality

--- frontend/src/containers/ReportPage/ReportPage.js
+++ frontend/src/containers/ReportPage/ReportPage.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import '../../assets/Report.css';
 
 function ReportPage() {
-  return (
-    <div className='report-page-container'>ReportPage</div>
-  )
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_k1cgql9', 'template_754ed7s', form.current, 'dmSGQByQIsdAnbDYF')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+    };
+
+    return (
+        <div className='report-page-container'>
+            
+            <form className='report-form' ref={form} onSubmit={sendEmail}>
+                <h1>Submit Bug Report</h1>
+                <select defaultValue={""} className='report-form-group' required type="text" name="issue" >
+                    <option value={""} disabled className='report-issue-option'>Select Issue Type...</option>
+                    <option className='report-issue-option'>Design</option>
+                    <option className='report-issue-option'>Broken Functionality</option>
+                    <option className='report-issue-option'>Data Not Updated</option>
+                    <option className='report-issue-option'>Account Issues</option>
+                    <option className='report-issue-option'>Disruptive User/Posted Content</option>
+                </select>
+
+                <input placeholder='Name' className='report-form-group' required type="text" name="name" />
+
+                <input placeholder='Email' className='report-form-group' required type="email" name="email" />
+
+                <textarea style={{'resize': 'none'}} placeholder='Details of bug' required name="message" />
+
+                <input className='report-submit-btn' type="submit" value="Submit" />
+            </form>
+            
+        </div>
+    )
 }
 
 export default ReportPage
