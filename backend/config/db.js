2c689b5613b9e0803ca542328214be47d49a89a8
SeanChinJunKai <seanchinjk1@gmail.com>
2022-12-14 11:41:39 +0800

    Fixing minor bugs

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 // function which connects to the MongoDB database via the uri given in .env file
 const connectDB = async () => {
     try {
+        console.log(`${process.env.MONGO_URI}`)
         const conn = await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
     } catch (error) {
